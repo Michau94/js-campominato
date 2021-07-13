4a77f41ede1052b89aaee256a30e89087cd1936e
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-13 17:43:16 +0200

    fix switch difficoltà

--- js/script.js
+++ js/script.js
@@ -43,11 +43,43 @@ var userChoice = [];
 //utilizzata per interromper ciclo while.
 var bomba = false;
 
+
+var maxNum = 0;
+
+// Richiesta selezione difficoltà
+
+var level = prompt('Easy, medium or hard').toLowerCase().trim();
+
+// Validazione difficoltà
+while (level != 'easy' && level != 'medium' && level != 'high') {
+    alert('Please insert correct difficulty');
+    level = prompt('Easy, medium or hard').toLowerCase().trim();
+}
+
+
+
+// cambio difficoltà attraverso switch
+switch (level) {
+    case 'easy':
+        maxNum = 100;
+        break;
+    case 'medium':
+        maxNum = 80;
+        break;
+    case 'high':
+        maxNum = 50;
+        break;
+}
+
+// stampa difficoltà scelta
+console.log('Livello di difficoltà scelto:', level);
+
 // generazione array bomba
-var i = 0;
 while (bombs.length < 16) {
 
-    var randomNumber = randomizeNumber(1, 100);
+    var randomNumber = randomizeNumber(1, maxNum);
+
+
     if (!bombs.includes(randomNumber)) {
         bombs.push(randomNumber);
     }
@@ -60,17 +92,18 @@ console.log(bombs);
 
 var i = 0;
 
+// includes lasciato apposta qui giusto per usare un po tutto 
 while (!bombs.includes(userNumber) || !userChoice.includes(userNumber) && bomba === false && i < (100 - 16)) {
 
     // se array bombe non iclude scelta utente e array scelta utente non include lo stesso numero continua a giocare
     var userNumber = parseInt(prompt('Inserisci un numero', 1));
 
-    while (validateNum(1, 100, userNumber) === false) {
-        alert('per favore inserire un numero compreso nel range')
+    while (validateNum(1, maxNum, userNumber) === false) {
+        alert('per favore inserire un numero compreso tra 1 e ' + maxNum)
         userNumber = parseInt(prompt('Inserisci un numero', 1));
     };
 
-    // se scelta già effettuata rischiedere numero
+    // se scelta già effettuata richiedere numero
     //userChoice.includes(userNumber) - soluzione easy
     while (arrayIncludes(userChoice, userNumber)) {
         userNumber = parseInt(prompt('Numero già inserito riprovare', 1));
@@ -81,6 +114,7 @@ while (!bombs.includes(userNumber) || !userChoice.includes(userNumber) && bomba 
     if (arrayIncludes(bombs, userNumber)) {
         bomba = true;
         alert('GameOver');
+        console.log('Game Over');
 
         // se array scelte utente contiene un numero gia scelto continuo a chiedere
     } else {
@@ -93,12 +127,14 @@ while (!bombs.includes(userNumber) || !userChoice.includes(userNumber) && bomba 
 
 console.log(userChoice);
 
+
+// output Punteggio
 console.log('Punteggio ottenuto: ', userChoice.length);
 
 
 
 
-// Functions 
+//! Functions 
 
 // Random Number 
 function randomizeNumber(min, max) {
