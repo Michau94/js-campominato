e9726dcc015f2843894b92629a986b60858633e4
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-14 14:53:17 +0200

    difficulty with select

--- js/script.js
+++ js/script.js
@@ -25,120 +25,163 @@ con difficoltà 2 => tra 1 e 50
 1-richiesta utente di un numero
 2- confronto numero con array bombe - se presente fine gioco game over
 3- se si continua confronto numero pushato in array numeri utente solo se non presente doppione
-3a - se presente numero in arrayscelteutente richiedere con while numero.
+3a - se presente numero in array scelte utente richiedere con while numero.
 4 - se numero passa sia controllo bombe che controllo doppione viene pushato.
 
 !stampa in console
 1- utilizzo della length di scelte utente come punteggio.
 
+Ristampa in html fatta come BONUS!
+
 
 */
-
+//! variabili
 // * display 
-var displayLevel = document.getElementById('difficulty');
-var pointsDisplay = document.getElementById('points');
-var infoMessage = document.getElementById('message');
-
-// array bombe di 16 cifre da 1 a 100 (cifre diverse)
-var bombs = [];
-
-// array per scelta utente
-var userChoice = [];
+let displayLevel = document.getElementById('difficulty');
+let pointsDisplay = document.getElementById('points');
+let infoMessage = document.getElementById('message');
+let playButton = document.getElementById('play');
+let gameOver = document.getElementById('over');
+let inputField = document.getElementById('number');
+let levelField = document.getElementById('level');
+let resetButton = document.getElementById('reset');
+let alertError = document.getElementById('alert');
+let confirmLevel = document.getElementById('confirm');
 
 //utilizzata per interromper ciclo while.
 var bomba = false;
 
+// array per scelta utente
+var userChoice = [];
 
+// variabile max per range difficoltà
 var maxNum = 0;
 
-// Richiesta selezione difficoltà
+var bombs = [];
 
-var level = prompt('Easy, medium or hard').toLowerCase().trim();
+
+// reset button
+resetButton.addEventListener('click', function () {
+
+    document.location.reload();
+
+})
+
+
+
+// Richiesta selezione difficoltà
+//var level = prompt('Easy, medium or hard').toLowerCase().trim();
 
 // Validazione difficoltà
-while (level != 'easy' && level != 'medium' && level != 'hard') {
-    alert('Please insert correct difficulty');
-    level = prompt('Easy, medium or hard').toLowerCase().trim();
-}
+// while (level != 'easy' && level != 'medium' && level != 'hard' || level === " " && !level) {
+//     alertError.innerText = 'Please insert correct difficulty';
+//     //alert('Please insert correct difficulty');
+//     level = prompt('Easy, medium or hard').toLowerCase().trim();
+
+// }
+
+// array bombe di 16 cifre da 1 a 100 (cifre diverse)
+playButton.addEventListener('click', function () {
 
 
+    let levelValue = levelField.value;
 
-// cambio difficoltà attraverso switch
-switch (level) {
-    case 'easy':
-        maxNum = 100;
-        break;
-    case 'medium':
-        maxNum = 80;
-        break;
-    case 'hard':
-        maxNum = 50;
-        break;
-}
-
-infoMessage.innerText = 'per favore inserire un numero compreso tra 1 e ' + maxNum;
-
-// stampa
-console.log('Livello di difficoltà scelto:', level);
-displayLevel.innerText = 'Livello di difficoltà scelto:' + level;
-
-// generazione array bomba
-while (bombs.length < 16) {
-
-    var randomNumber = randomizeNumber(1, maxNum);
-
-    if (!bombs.includes(randomNumber)) {
-        bombs.push(randomNumber);
-    }
-    i++;
-}
-
-// generato array di 16 cifre
-console.log(bombs);
-
-
-var i = 0;
-
-// includes lasciato apposta qui giusto per usare un po tutto 
-while (!bombs.includes(userNumber) || !userChoice.includes(userNumber) && bomba === false && i < (100 - 16)) {
-
-    // se array bombe non iclude scelta utente e array scelta utente non include lo stesso numero continua a giocare
-    var userNumber = parseInt(prompt('Inserisci un numero' + 'tentativi restanti: ' + (85 - i), 1));
-
-    while (validateNum(1, maxNum, userNumber) === false) {
-        infoMessage.innerText = 'per favore inserire un numero compreso tra 1 e ' + maxNum;
-        //alert('per favore inserire un numero compreso tra 1 e ' + maxNum)
-        userNumber = parseInt(prompt('per favore inserire un numero compreso tra 1 e ' + maxNum, 1));
-    };
-
-    // se scelta già effettuata richiedere numero
-    //userChoice.includes(userNumber) - soluzione easy
-    while (arrayIncludes(userChoice, userNumber)) {
-        userNumber = parseInt(prompt('Numero già inserito riprovare', 1));
+    // cambio difficoltà attraverso switch
+    switch (levelValue) {
+        case 'easy':
+            maxNum = 100;
+            break;
+        case 'medium':
+            maxNum = 80;
+            break;
+        case 'hard':
+            maxNum = 50;
+            break;
     }
 
-    // se bombs include scelta utente il gioco finisce GAME OVER
-    //bombs.includes(userNumber) - soluzione easy
-    if (arrayIncludes(bombs, userNumber)) {
-        bomba = true;
-        alert('GameOver');
-        console.log('Game Over');
 
-        // se array scelte utente contiene un numero gia scelto continuo a chiedere
-    } else {
-        userChoice.push(userNumber);
+    // stampa difficoltà
+
+    console.log('Livello di difficoltà scelto:', levelValue);
+    displayLevel.innerText = 'Livello di difficoltà scelto:' + levelValue;
+
+    // generazione array bomba
+
+    while (bombs.length < 16) {
+
+        var randomNumber = randomizeNumber(1, maxNum);
+
+        if (!bombs.includes(randomNumber)) {
+            bombs.push(randomNumber);
+        }
+
     }
 
-    i++;
-}
+    // generato array di 16 cifre
+    console.log(bombs);
+
+    if (maxNum < 0) {
+        alert('selezionare difficoltà e selezionare conferma')
+    }
+
+    alertError.classList.add('hidden');
+
+    var userNumber = parseInt(inputField.value);
+    console.log(userNumber);
+
+    var i = 0;
+
+    // includes lasciato apposta qui giusto per usare un po tutto 
+    // se array bombe non include scelta utente e array scelta utente non include lo stesso numero continua a giocare
+    if (bomba === false && i < (100 - 16)) {
+
+        //var userNumber = parseInt(prompt('Inserisci un numero' + 'tentativi restanti: ' + (85 - i), 1));
+        validateNum(1, maxNum, userNumber);
+        if (validateNum(1, maxNum, userNumber) === false) {
+
+            alertError.classList.remove('hidden');
+            alertError.classList.add('red');
+            alertError.innerText = 'per favore inserire un numero compreso tra 1 e ' + maxNum;
+            //!userNumber = parseInt(prompt('per favore inserire un numero compreso tra 1 e ' + maxNum, 1));
+        } else if (arrayIncludes(userChoice, userNumber)) {
+            // se array scelte utente contiene un numero gia scelto continuo a chiedere
+
+            //!userChoice.includes(userNumber) - soluzione easy
+
+            alertError.classList.remove('hidden');
+            alertError.innerText = 'Numero già inserito per favore riprovare';
+
+            //alert('Numero già inserito per favore riprovare')
+
+        } else if (arrayIncludes(bombs, userNumber)) {
+            // se bombs include scelta utente il gioco finisce GAME OVER
+
+            //bombs.includes(userNumber) - soluzione easy
+
+            bomba = true;
+
+            //alert('GameOver');
+            console.log('Game Over');
+            gameOver.innerText = 'GAME OVER!'
 
 
-console.log(userChoice);
+        } else {
+
+            //se passa tutti i controlli numero pushato (punto guadagnato)
+            userChoice.push(userNumber);
+            inputField.value = " ";
+        }
+
+        i++;
+
+    }
 
 
-// output Punteggio
-console.log('Punteggio ottenuto: ', userChoice.length);
-pointsDisplay.innerText = 'Punteggio ottenuto: ' + userChoice.length;
+    // output Punteggio
+    console.log('Punteggio ottenuto: ', userChoice.length);
+    pointsDisplay.innerText = 'Punteggio ottenuto: ' + userChoice.length;
+
+})
 
 
 
